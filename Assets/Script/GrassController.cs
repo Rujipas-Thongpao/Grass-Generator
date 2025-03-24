92997b6d7b44dde1e39ede11b930d34773e041a1
paopeaw <paopeaw8246@gmail.com>
2025-03-24 18:54:50 +0700

    initial commit

---
+++ Assets/Script/GrassController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class GrassController : MonoBehaviour
+{
+    [SerializeField]
+    private int a = 10;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("hello world");
+    }
+}
